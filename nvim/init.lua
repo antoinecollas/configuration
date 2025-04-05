63884ed13fa20fe22bdba498e898700958f94986
Antoine Collas <contact@antoinecollas.fr>
2025-04-05 10:02:25 +0200

    neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,45 +21,50 @@ vim.g.maplocalleader = "\\"
 
 -- Setup plugins
 require("lazy").setup({
-  spec = {
-    -- === Theme ===
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      priority = 1000,
-      config = function()
-        vim.cmd.colorscheme("catppuccin-mocha")
-      end,
-    },
+	spec = {
+		-- === Theme ===
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			config = function()
+				vim.cmd.colorscheme("catppuccin-mocha")
+			end,
+		},
 
-    -- === Copilot ===
-    {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      build = ":Copilot auth",
-      config = function()
-        require("copilot").setup({
-          suggestion = { enabled = true, auto_trigger = true },
-          panel = { enabled = false },
-        })
-      end,
-    },
+		-- === Copilot ===
+		{
+			"zbirenbaum/copilot.lua",
+			cmd = "Copilot",
+			build = ":Copilot auth",
+			config = function()
+				require("copilot").setup({
+					suggestion = { enabled = true, auto_trigger = true },
+					panel = { enabled = false },
+				})
+			end,
+		},
 
-    -- === LSP ===
-    { "neovim/nvim-lspconfig" },
+		-- === LSP ===
+		{ "neovim/nvim-lspconfig" },
 
-    -- === Autocompletion ===
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "L3MON4D3/LuaSnip" },
-    { "saadparwaiz1/cmp_luasnip" },
-  },
-  install = { colorscheme = { "catppuccin" } },
-  checker = { enabled = true },
-  rocks = { enabled = false },
+		-- === Autocompletion ===
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "L3MON4D3/LuaSnip" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "ray-x/lsp_signature.nvim",
+		config = function()
+			require("lsp_signature").setup({})
+		end,
+	}
+},
+install = { colorscheme = { "catppuccin" } },
+checker = { enabled = true },
+rocks = { enabled = false },
 })
 
 -- === Editor Options ===
@@ -92,13 +97,13 @@ map("n", "<F7>", ":cp<CR>", opts)
 map("n", "<F8>", ":cn<CR>", opts)
 vim.cmd([[iabbrev ipdb import ipdb; ipdb.set_trace()]])
 vim.api.nvim_create_autocmd("VimResized", {
-  callback = function() vim.cmd("wincmd =") end,
+	callback = function() vim.cmd("wincmd =") end,
 })
 
 -- === Copilot Keybinding ===
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', {
-  expr = true, silent = true, noremap = true,
+	expr = true, silent = true, noremap = true,
 })
 
 -- === LSP: Python support ===
@@ -107,19 +112,19 @@ require("lspconfig").pyright.setup({})
 -- === nvim-cmp setup ===
 local cmp = require("cmp")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  }, {
-    { name = "buffer" },
-  }),
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
 })
