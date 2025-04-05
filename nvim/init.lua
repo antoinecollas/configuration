e11bb5ea26c43688f313f3f709f6d7272e4e50fa
Antoine Collas <contact@antoinecollas.fr>
2025-04-05 10:08:31 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,7 +28,57 @@ require("lazy").setup({
 			name = "catppuccin",
 			priority = 1000,
 			config = function()
-				vim.cmd.colorscheme("catppuccin-mocha")
+				require("catppuccin").setup({
+					flavour = "auto", -- latte, frappe, macchiato, mocha
+					background = { -- :h background
+						light = "latte",
+						dark = "mocha",
+					},
+					transparent_background = false, -- disables setting the background color.
+					show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+					term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+					dim_inactive = {
+						enabled = false, -- dims the background color of inactive window
+						shade = "dark",
+						percentage = 0.15, -- percentage of the shade to apply to the inactive window
+					},
+					no_italic = false, -- Force no italic
+					no_bold = false, -- Force no bold
+					no_underline = false, -- Force no underline
+					styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+						comments = { "italic" }, -- Change the style of comments
+						conditionals = { "italic" },
+						loops = {},
+						functions = {},
+						keywords = {},
+						strings = {},
+						variables = {},
+						numbers = {},
+						booleans = {},
+						properties = {},
+						types = {},
+						operators = {},
+						-- miscs = {}, -- Uncomment to turn off hard-coded styles
+					},
+					color_overrides = {},
+					custom_highlights = {},
+					default_integrations = true,
+					integrations = {
+						cmp = true,
+						gitsigns = true,
+						nvimtree = true,
+						treesitter = true,
+						notify = false,
+						mini = {
+							enabled = true,
+							indentscope_color = "",
+						},
+						-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+					},
+				})
+
+				-- setup must be called before loading
+				vim.cmd.colorscheme "catppuccin"
 			end,
 		},
 
@@ -56,15 +106,18 @@ require("lazy").setup({
 		{ "hrsh7th/cmp-cmdline" },
 		{ "L3MON4D3/LuaSnip" },
 		{ "saadparwaiz1/cmp_luasnip" },
-		{ "ray-x/lsp_signature.nvim",
-		config = function()
-			require("lsp_signature").setup({})
-		end,
-	}
-},
-install = { colorscheme = { "catppuccin" } },
-checker = { enabled = true },
-rocks = { enabled = false },
+		{
+			"ray-x/lsp_signature.nvim",
+			config = function()
+				require("lsp_signature").setup({
+					hint_prefix = "→ ",  -- nice subtle arrow
+				})
+			end,
+		}
+	},
+	install = { colorscheme = { "catppuccin" } },
+	checker = { enabled = true },
+	rocks = { enabled = false },
 })
 
 -- === Editor Options ===
