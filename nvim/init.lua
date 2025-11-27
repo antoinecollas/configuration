b06cbfe8dab6b9f8976b494fbddb6148ce595b66
Antoine Collas <contact@antoinecollas.fr>
2025-11-27 14:34:14 +0100

    Explore

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,7 @@ vim.opt.termguicolors = true
 -- === Keymaps ===
 vim.keymap.set("n", "<leader>/", ":noh<CR>", { silent = true }) -- Clear search highlights
 vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle!<CR>") -- Outline
+vim.api.nvim_create_user_command("E", "Explore", {})
 
 -- Standard Window Navigation (Ctrl + h/j/k/l)
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Window Left" })
